c11f8a0701e7c24e4faa3804a9ea46ba40c9fb41
stfnrm <77114687+stfnrm@users.noreply.github.com>
2023-07-11 20:38:44 +0800

    imrpoved UX and added first templates

--- website/script.js
+++ website/script.js
@@ -1,35 +1,8 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const menuItems = document.querySelectorAll("nav ul li");
-
-//   menuItems.forEach(function (item) {
-//     item.addEventListener("click", function () {
-//       // Remove 'active' class from all items
-//       menuItems.forEach(function (item) {
-//         item.classList.remove("active");
-//       });
-
-//       // Add 'active' class to the clicked item
-//       this.classList.add("active");
-//     });
-//   });
-// });
-
-// // Slideshow functionality
-// const slideshowImages = ["image1.jpg", "image2.jpg", "image3.jpg"];
-// let currentImageIndex = 0;
-
-// function changeSlideshowImage() {
-//   const slideshowSection = document.querySelector(".slideshow");
-//   slideshowSection.style.backgroundImage = `url(${slideshowImages[currentImageIndex]})`;
-//   currentImageIndex = (currentImageIndex + 1) % slideshowImages.length;
-// }
-
-// setInterval(changeSlideshowImage, 5000);
-
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function toggleMegaMenu() {
   document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("menu-background").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
@@ -43,5 +16,67 @@ window.onclick = function (event) {
         openDropdown.classList.remove("show");
       }
     }
+    var backgrounds = document.getElementsByClassName("menu-background");
+    var i;
+    for (i = 0; i < backgrounds.length; i++) {
+      var openBackground = backgrounds[i];
+      if (openBackground.classList.contains("show")) {
+        openBackground.classList.remove("show");
+      }
+    }
   }
 };
+
+// Slideshow
+
+let slideIndex = 1;
+showSlides(slideIndex);
+// slideshowAutomation();
+let timeoutHandle = setTimeout(slideshowAutomation, 2000); // Change image every 2 seconds
+
+// Next/previous controls
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
+
+// Thumbnail image controls
+function currentSlide(n) {
+  clearTimeout(timeoutHandle);
+  showSlides((slideIndex = n));
+  timeoutHandle = setTimeout(slideshowAutomation, 2000); // Change image every 2 seconds
+}
+
+function slideshowAutomation() {
+  slideIndex++;
+  showSlides(slideIndex);
+  timeoutHandle = setTimeout(slideshowAutomation, 2000); // Change image every 2 seconds
+}
+
+// function slideshowAutomation() {
+//   let dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < dots.length; i++) {
+//     if (dots[i].classList.contains("active")) {
+//       showSlides((slideIndex = i + 2));
+//     }
+//   }
+// }
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
